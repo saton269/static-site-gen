92d84620c2e991d9507658095e34842605aaacc0
saton269 <saton269@gmail.com>
2025-07-13 20:14:56 -0400

    fleshed out generator to generate single page

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -30,10 +30,11 @@ class LeafNode(HTMLNode):
         if self.tag is None:
             return self.value
         return f"<{self.tag}{self.props_to_html()}>{self.value}</{self.tag}>"
-    
+
     def __repr__(self):
         return f"LeafNode({self.tag}, {self.value}, {self.props})"
-    
+
+
 class ParentNode(HTMLNode):
     def __init__(self, tag, children, props=None):
         super().__init__(tag, None, children, props)
@@ -47,8 +48,6 @@ class ParentNode(HTMLNode):
         for child in self.children:
             children_html += child.to_html()
         return f"<{self.tag}{self.props_to_html()}>{children_html}</{self.tag}>"
-        
 
     def __repr__(self):
         return f"ParentNode({self.tag}, children: {self.children}, {self.props})"
-        
--- src/inline_markdown.py
+++ src/inline_markdown.py
@@ -2,6 +2,7 @@ import re
 
 from textnode import TextNode, TextType
 
+
 def text_to_textnodes(text):
     nodes = [TextNode(text, TextType.TEXT)]
     nodes = split_nodes_delimiter(nodes, "**", TextType.BOLD)
@@ -11,16 +12,17 @@ def text_to_textnodes(text):
     nodes = split_nodes_link(nodes)
     return nodes
 
-def split_nodes_delimiter(old_nodes, delimimiter, text_type):
+
+def split_nodes_delimiter(old_nodes, delimiter, text_type):
     new_nodes = []
     for old_node in old_nodes:
         if old_node.text_type != TextType.TEXT:
             new_nodes.append(old_node)
             continue
         split_nodes = []
-        sections = old_node.text.split(delimimiter)
+        sections = old_node.text.split(delimiter)
         if len(sections) % 2 == 0:
-            raise ValueError("invlaid markdown, formatted section not closed")
+            raise ValueError("invalid markdown, formatted section not closed")
         for i in range(len(sections)):
             if sections[i] == "":
                 continue
@@ -31,16 +33,6 @@ def split_nodes_delimiter(old_nodes, delimimiter, text_type):
         new_nodes.extend(split_nodes)
     return new_nodes
 
-
-def extract_markdown_images(text):
-    pattern = r"!\[([^\[\]]*)\]\(([^\(\)]*)\)"
-    matches = re.findall(pattern, text)
-    return matches
-
-def extract_markdown_links(text):
-    pattern = r"(?<!!)\[([^\[\]]*)\]\(([^\(\)]*)\)"
-    matches = re.findall(pattern, text)
-    return matches
 
 def split_nodes_image(old_nodes):
     new_nodes = []
@@ -71,6 +63,7 @@ def split_nodes_image(old_nodes):
             new_nodes.append(TextNode(original_text, TextType.TEXT))
     return new_nodes
 
+
 def split_nodes_link(old_nodes):
     new_nodes = []
     for old_node in old_nodes:
@@ -93,3 +86,15 @@ def split_nodes_link(old_nodes):
         if original_text != "":
             new_nodes.append(TextNode(original_text, TextType.TEXT))
     return new_nodes
+
+
+def extract_markdown_images(text):
+    pattern = r"!\[([^\[\]]*)\]\(([^\(\)]*)\)"
+    matches = re.findall(pattern, text)
+    return matches
+
+
+def extract_markdown_links(text):
+    pattern = r"(?<!!)\[([^\[\]]*)\]\(([^\(\)]*)\)"
+    matches = re.findall(pattern, text)
+    return matches
